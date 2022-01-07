3aa667a45586ee438988dd7a259a29e0599f6eae
Daniel Zamanillo <dzamanillok@gmail.com>
2022-01-07 16:37:07 -0600

    pages/projects: simple card created with foreach. still need to add images

--- pages/projects.php
+++ pages/projects.php
@@ -11,6 +11,28 @@
 <body>
     <?php require('../templates/header.php'); ?>
 
+    <div class="container d-flex justify-content-center flex-wrap ">
+
+    <?php
+        $projectsFile = '../projects.json';
+        $string = file_get_contents($projectsFile);
+        $json = json_decode($string);
+
+        foreach ($json as $project){
+            echo 
+            "<div class='card m-3' style='width: 18rem;'>
+                <div class='card-body'>
+                    <h5 class='card-title'>" . $project->name . "</h5>
+                    <p class='card-text'>" . $project->description . "</p>
+                    <a href='" . $project->gitLink . "' class='card-link' target='_blank'>Github</a>
+                    <a href='" . $project->deployLink . "' class='card-link' target='_blank'>Website</a>
+                </div>
+            </div>";
+        }
+
+    ?>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
