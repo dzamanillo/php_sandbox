7eaf0e7860e2db1c0c52128bf8bf0f4f024cfaab
Daniel Zamanillo <dzamanillok@gmail.com>
2022-01-07 15:53:14 -0600

    test fetch

--- index.php
+++ index.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Work!</h1>
+    <h1 class="text-primary test">Work!</h1>
+    <?php
+        $url = 'projects.json';
+        $request = file_get_contents($url);
+        $json = json_decode($request);
+        foreach ($json as $i) {
+            echo "<p>" . $i->name . "</p>";
+        }
+        
+        
+    
+
+    ?>
 </body>
 </html>
